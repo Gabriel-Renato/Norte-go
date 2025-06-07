65af28b906cf0a1037faa98d001fb7b935cbc2ba
Gabriel Renato Souza de Araújo <gabrielrenatosouzadearaujo@gmail.com>
2025-06-07 13:46:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function apagartexto() {
+  const campo1 = document.querySelector("#name");
+  const campo2 = document.querySelector("#email");
+  const campo3 = document.querySelector("#message");
+  campo1.value = "";
+  campo2.value = "";
+  campo3.value = "";
+}
+  
